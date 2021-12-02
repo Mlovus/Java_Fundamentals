a27d15f26baf436c7026d3ed53154c97f6870396
Max Lovus <mlovus@gmail.com>
2021-12-02 14:10:59 -0800

    completed example 1

--- src/labs_examples/arrays/labs/Exercise_01.java
+++ src/labs_examples/arrays/labs/Exercise_01.java
@@ -1,15 +1,29 @@
 package labs_examples.arrays.labs;
-
+import java.util.Scanner;
 /**
  * Arrays calculator
  *
  *      Take in 10 numbers from the user. Place the numbers in an array. Using the loop of your choice,
- *      calculate the sum of all of the numbers in the array as well as the average.
+ *      calculate the sum of all the numbers in the array as well as the average.
  *
  *      Print the results to the console.
  *
  */
 
 public class Exercise_01 {
-
+    public static void main(String[] args){
+        int[] sum = new int[10];
+        int total=0;
+        Scanner input = new Scanner(System.in);
+        for(int i=0; i< sum.length; i++){
+            System.out.println("Please Input a number");
+            sum[i]=input.nextInt();
+            System.out.println(sum[i]);
+        }
+        input.close();
+        for (int i=0; i< sum.length; i++) {
+            total += sum[i];
+        }
+        System.out.println("The total of all numbers is: "+total+" The average of all the numbers is: "+ (new Double (total)/new Double (sum.length)));
+    }
 }
