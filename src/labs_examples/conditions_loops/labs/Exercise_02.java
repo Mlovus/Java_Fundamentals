d97cc2588c15114433fcde5c99c9b57502ec66c1
Max Lovus <mlovus@gmail.com>
2021-12-06 13:30:28 -0800

    Completed conditional loops exercise 2

--- src/labs_examples/conditions_loops/labs/Exercise_02.java
+++ src/labs_examples/conditions_loops/labs/Exercise_02.java
@@ -1,5 +1,7 @@
 package labs_examples.conditions_loops.labs;
 
+import java.util.Scanner;
+
 /**
  * Conditions and Loops Exercise 2: Days of the week
  *
@@ -14,7 +16,37 @@ package labs_examples.conditions_loops.labs;
 public class Exercise_02 {
 
     public static void main(String[] args) {
-
+        Scanner input = new Scanner(System.in);
+        System.out.println("Input a number");
+        int number = input.nextInt();
+        if(number>0&&number<8){
+            switch (number) {
+                case 1:
+                    System.out.println("Monday");
+                    break;
+                case 2:
+                    System.out.println("Tuesday");
+                    break;
+                case 3:
+                    System.out.println("Wednesday");
+                    break;
+                case 4:
+                    System.out.println("Thursday");
+                    break;
+                case 5:
+                    System.out.println("Friday");
+                    break;
+                case 6:
+                    System.out.println("Saturday");
+                    break;
+                case 7:
+                    System.out.println("Sunday");
+                    break;
+            }
+        }
+        else{
+            System.out.println("Other");
+        }
         // 1) create scanner (don't forget to import Scanner!)
         // 2) prompt user
         // 3) assign input to variable as int
