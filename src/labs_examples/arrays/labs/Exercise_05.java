d5f05362afa19f3ac5a1eb7dda6d99a0cdb96d8a
Max Lovus <mlovus@gmail.com>
2021-12-06 13:05:36 -0800

    Completed exercise 5

--- src/labs_examples/arrays/labs/Exercise_05.java
+++ src/labs_examples/arrays/labs/Exercise_05.java
@@ -1,5 +1,8 @@
 package labs_examples.arrays.labs;
 
+import java.util.Arrays;
+import java.util.Collections;
+
 /**
  *  Traversing Arrays Backwards
  *
@@ -9,5 +12,19 @@ package labs_examples.arrays.labs;
  */
 
 public class Exercise_05 {
+    public static void main(String[] args){
+        int[]array = new int[10];
+        for (int i=0; i<array.length; i++){
+                array[i]=(int)(Math.random()*100);
+            }
 
+        for (int i = array.length-1; i >=0 ; i--) {
+            if((array.length-i+1)%2==0){
+                System.out.print(array[i]+" ");
+            }
+        }
+
+    }
 }
+
+
