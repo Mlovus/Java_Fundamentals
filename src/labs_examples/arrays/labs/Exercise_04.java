3e1297527a7514690524d5a414921736a18ad5f6
Max Lovus <mlovus@gmail.com>
2021-12-05 21:17:32 -0800

    Completed exercise 4

--- src/labs_examples/arrays/labs/Exercise_04.java
+++ src/labs_examples/arrays/labs/Exercise_04.java
@@ -9,5 +9,16 @@ package labs_examples.arrays.labs;
  */
 
 public class Exercise_04 {
+    public static void main(String[] args) {
+        int[][] array = new int[5][5];
 
+        for (int i=0; i<array.length; i++){
+            for(int j=0; j<array[i].length; j++){
+                array[i][j]=(int)(Math.random()*100);
+                }
+            }
+        for(int[] i:array)
+            for(int j:i)
+                System.out.println(j);
+    }
 }
